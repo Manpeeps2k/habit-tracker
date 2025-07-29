1bebbc609facbb3ff89d564114a4c1af86611d1e
Manpeeps2k <216877517+Manpeeps2k@users.noreply.github.com.>
2025-07-30 00:14:36 +0800

    added different functions to different options

--- main.py
+++ main.py
@@ -1,28 +1,74 @@
 import json
 from datetime import date
 
+# functions
 
+habits = {
+
+}
+def showlist(habits):
+    print("this is the list of habits")
+    print(habits)
+
+def load_habits():
+    try:
+        with open('habits.json', 'r') as file:
+            return json.load(file)
+    except FileNotFoundError:
+        return {}
+    
+habits = load_habits
+
+def save_habits():
+    pass
+
+
+def remove_habit(habits):
+    removehabit = input(f"remove a habit: {habits}")
+    if removehabit not in habits:
+        print(f"please remove among the options: {habits}")
+    else:
+        del habits[removehabit]
+        print(f"removed {removehabit} from the list of habits")
+    
+
+def add_habit(habits):
+    addedhabit = input("add a habit").strip()
+    if addedhabit in habits:
+        print("habit name already exists")
+    else:
+        habits[addedhabit] = []
+        print(f"the habit: {addedhabit} is added.")
+    
+        
+    
 
 
 # adding the different options
-print("Habit Tracker")
-print("1. Add a habit")
-print("2. Remove a habit")
-print("3. View your habits")
-print("4. Exit the program")
 
-options = input("please select one of the options: ")
+while True:
+    print("Habit Tracker")
+    print("1. Add a habit")
+    print("2. Remove a habit")
+    print("3. View your habits")
+    print("4. Save your habit")
+    print("5. Exit the program")
 
-if options == "1":
+    options = input("please select one of the options: ")
 
-elif options == "2":
+    if options == "1":
 
-elif options == "3":
+    elif options == "2":
 
-elif options == "4":
-    
-else:
-    print("please input a number")
+    elif options == "3":
 
-if __name__ == "__main__":
-    main()
+    elif options == "4":
+
+    elif options == "5":
+        print("Thank You and Goodbye")
+        break
+    else:
+        print("please input a number")
+
+    if __name__ == "__main__":
+        main()
