c180da7cb99f18c6605e295d8e1da97a2f671c79
Manpeeps2k <216877517+Manpeeps2k@users.noreply.github.com.>
2025-07-29 22:27:04 +0800

    added the different options available but with no function yet

---
+++ main.py
@@ -0,0 +1,28 @@
+import json
+from datetime import date
+
+
+
+
+# adding the different options
+print("Habit Tracker")
+print("1. Add a habit")
+print("2. Remove a habit")
+print("3. View your habits")
+print("4. Exit the program")
+
+options = input("please select one of the options: ")
+
+if options == "1":
+
+elif options == "2":
+
+elif options == "3":
+
+elif options == "4":
+    
+else:
+    print("please input a number")
+
+if __name__ == "__main__":
+    main()
